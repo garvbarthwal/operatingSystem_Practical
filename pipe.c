d2ef239ec24d655cb82a9832bc044840733b5422
Garv <garvbarthwal@gmail.com>
2025-11-25 01:40:17 +0530

    Improved pipe

--- pipe.c
+++ pipe.c
@@ -1,30 +1,48 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/wait.h>
-#include<string.h>
-#include<sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(){
-
+int main() {
     int fd[2];
     char buffer[100];
-    printf("Enter input string:");
-    scanf("%[^\n]",buffer);
 
-    pipe(fd);
-    pid_t p=fork();
-    if(p > 0){
-        printf("\nWriting on the pipe\n");
-        printf("No of bytes : %d\n",(int)strlen(buffer));
-        write(fd[1],buffer,strlen(buffer));
-    } else if(p == 0){
-        printf("\nReading from the pipe\n");
-        int n=read(fd[0],buffer,strlen(buffer));
-        printf("Msg Received: %s\n",buffer);
-        printf("No of bytes received: %d",n);
-    } else {
-        printf("Fork() sys call failed");
+    printf("Enter a string: ");
+    if(!fgets(buffer,sizeof(buffer),stdin)){
+        perror("write");
+        exit(1);
     }
-    return 0; 
+
+    if(pipe(fd) == -1){
+        perror("pipe");
+        exit(1);
+    }
+
+    pid_t p=fork();
+    if(p < 0){
+        printf("FOrk failed");
+        exit(1);
+    }
+
+    if(p > 0){
+        close(fd[0]);
+
+        int len=strlen(buffer);
+        write(fd[1],buffer,len);
+        printf("Writer.Writing into the pipe.\n");
+        printf("No of bytes: %d\n",len);
+        close(fd[1]);
+        wait(NULL);
+    } else {
+        close(fd[1]);
+        int len=read(fd[0],buffer,sizeof(buffer)-1);
+        buffer[len]='\0';
+        printf("In reader. Reading the pipe\n");
+        printf("Msg received: %s", buffer);
+        printf("No of bytes: %d\n", len);
+        close(fd[0]);
+    }
+    return 0;
 }
